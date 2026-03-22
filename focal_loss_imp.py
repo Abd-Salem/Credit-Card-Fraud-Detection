8a01ae6fa6772a9a4b59cf359767b1b12866b1ae
Abdallah <abdallah.ashraf854@gmail.com>
2026-03-22 13:47:10 +0200

    nn model with focal loss implementation

--- focal_loss_imp.py
+++ focal_loss_imp.py
@@ -1,14 +1,12 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as f
-from configs import Config
-import numpy as np
-
-config = Config()
-
 
 
 class FocalLoss(nn.Module):
+    '''
+    focal loss class implementation
+    '''
     def __init__(self,alpha=0.25, gamma=2):
         super().__init__()
         self.alpha = alpha
@@ -22,11 +20,14 @@ class FocalLoss(nn.Module):
         return loss.mean()
 
 class MLP_FL(nn.Module):
+    '''
+    neural network class implementation
+    '''
     def __init__(self, input_dim, hidden_layers=(128,64), dropout=0.2):
         super().__init__()
         in_dim = input_dim
         layers = []
-        for h in layers:
+        for h in hidden_layers:
             layers.extend([
                 nn.Linear(in_dim, h),
                 nn.BatchNorm1d(h),
@@ -39,29 +40,3 @@ class MLP_FL(nn.Module):
 
     def forward(self, x):
         return self.net(x).squeeze(-1)
-
-
-def train(model, x_train, t_train, criterion, optimizer):
-    model.train()
-    optimizer.zero_grad()
-    logits = model(x_train)
-    loss = criterion(logits, t_train)
-    loss.backward()
-    optimizer.step()
-    return loss.item()
-
-
-def predict(model, x_train):
-    model.eval()
-    with torch.no_grad():
-        logits = model(x_train)
-        proba = torch.sigmoid(logits)
-    return np.array(proba)
-
-
-def evaluate(model, x_train, t_train, criterion):
-    model.eval()
-    with torch.no_grad():
-        logits = model(x_train)
-        loss = criterion(logits, t_train)
-    return loss.item()
