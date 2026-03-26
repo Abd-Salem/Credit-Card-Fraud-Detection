3b10b99eb0690beb66aad5f7e8627477484fd466
Abdallah <abdallah.ashraf854@gmail.com>
2026-03-26 20:25:32 +0200

    add val data to sampled data for future adding to train data

--- credit_fraud_utils_data.py
+++ credit_fraud_utils_data.py
@@ -1,14 +1,11 @@
 import json
 import pandas as pd
 import numpy as np
-from configs import Config
+from credit_fraud_utils_helper import get_processed_data
+from load_configs import Config
 from imblearn.over_sampling import SMOTE
 from imblearn.under_sampling import RandomUnderSampler, EditedNearestNeighbours
 from imblearn.combine import SMOTEENN, SMOTETomek
-
-
-# create config loader
-config = Config()
 
 def load_data(path: str):
     '''
@@ -75,18 +72,12 @@ def prepare_data(load_path:str='', target_preparing_path:str='', preparing_meta_
     x = X.to_numpy()
     y = y.to_numpy()
 
-    # dict for saving data
-    data = {
-        'x_prepared': x,
-        'y_prepared': y
-    }
-
     # save data in .npz file
-    np.savez_compressed(target_preparing_path, **data)
+    np.savez_compressed(target_preparing_path, x=x, y=y)
 
     # metadata of prepared data
     metadata= {
-        'input_cols_names' : X.columns.tolist(),
+        'cols_names' : X.columns.tolist(),
         'n_samples': X.shape[0],
         'n_features': X.shape[1],
     }
@@ -95,82 +86,82 @@ def prepare_data(load_path:str='', target_preparing_path:str='', preparing_meta_
         json.dump(metadata, f, indent=4)
 
 
-def sample_save_data(load_prepared_path:str='', load_meta_path:str='',
-                     split_name:str = ''):
+def sample_save_data(load_prepared_path:str='', meta_path:str='',config=Config()):
     '''
     apply all sample techniques for train split data and save for modeling fits
     :param load_prepared_path: where prepared data saved
-    :param load_meta_path: where metadata of prepared data saved
-    :param split_name: name to get save path
+    :param meta_path: path of metadata that contain cols names
+    :param config: set configurations
     :return: None
     '''
 
-    # get prepared data from given paths
-    data = np.load(load_prepared_path)
-
-    with open(load_meta_path, 'r') as f:
-        meta = json.load(f)
-
-    X = data['x_prepared']
-    y = data['y_prepared']
-    cols_names = meta['input_cols_names']       # columns names
-
-    # change to dataframe type
-    X, y = pd.DataFrame(data=X, columns=cols_names), pd.Series(y)
+    X, t = get_processed_data(data_path=load_prepared_path, dtype='df', meta_path=meta_path)
 
     # get our configs
     kn = config.SAMPLING['params']['smote_kn']
     nn = config.SAMPLING['params']['enn_nn']
     sampling_strategy = config.SAMPLING['params']['sampling_strategy']
+    random_state = config.RANDOM_STATE
+    techniques = config.SAMPLING['techniques']
 
+
+    # map different samplers in dict
     sampler_map = {
-        'rus': RandomUnderSampler(random_state=config.RANDOM_STATE, sampling_strategy=sampling_strategy),
+        'rus': RandomUnderSampler(random_state=random_state, sampling_strategy=sampling_strategy),
         'enn': EditedNearestNeighbours(sampling_strategy=sampling_strategy, n_neighbors=nn),
-        'smote': SMOTE(random_state=config.RANDOM_STATE, sampling_strategy=sampling_strategy, k_neighbors=kn),
-        'smoteenn': SMOTEENN(random_state=config.RANDOM_STATE,
+        'smote': SMOTE(random_state=random_state, sampling_strategy=sampling_strategy, k_neighbors=kn),
+        'smoteenn': SMOTEENN(random_state=random_state,
                              sampling_strategy=sampling_strategy,
                              smote=SMOTE(k_neighbors=kn), enn=EditedNearestNeighbours(n_neighbors=nn)),
-        'smotetomek': SMOTETomek(random_state=config.RANDOM_STATE ,sampling_strategy=sampling_strategy,
+        'smotetomek': SMOTETomek(random_state=random_state ,sampling_strategy=sampling_strategy,
                              smote=SMOTE(k_neighbors=kn))
     }
 
-    # dict of samples processes
-    samplers = {technique_name: sampler_map[technique_name] for technique_name in config.SAMPLING['techniques']}
+    # dict of samples according to configurations
+    samplers = {technique_name: sampler_map[technique_name] for technique_name in techniques}
 
     # apply all techniques on loaded data and save processed data in files
     for name, sampler in samplers.items():
 
-        x_sampled, y_sampled = sampler.fit_resample(X, y)  # get sampled data
+        x_sampled, y_sampled = sampler.fit_resample(X, t)  # get sampled data
+
+        meta = {
+            'cols_names' : X.columns.to_list(),
+            'n_features'    : X.shape[0],
+            'n_samples'     : X.shape[1]
+        }
 
         # save sampled data in .npz file
-        sampled = {
-            'x_sampled': x_sampled,
-            'y_sampled': y_sampled
-        }
-        np.savez_compressed(f'{config.DATASET['sampled'][name][split_name]}', **sampled)
+        data_save_path = None
+        metadata_save_path = None
+        if 'train'  in load_prepared_path:
+            data_save_path = config.DATASET['sampled'][name]['train']['data']
+            metadata_save_path = config.DATASET['sampled'][name]['train']['metadata']
+        elif 'val' in load_prepared_path:
+            data_save_path = config.DATASET['sampled'][name]['val']['data']
+            metadata_save_path = config.DATASET['sampled'][name]['val']['metadata']
 
-        # save metadata
-        sample_metadata = {
-            'input_cols_names': X.columns.tolist(),
-            'num_samples': X.shape[0],
-            'num_features': X.shape[1]
-        }
-        with open(f'{config.DATASET['sampled'][name][f'{split_name}_metadata']}', 'w') as f:
-            json.dump(sample_metadata, f, indent=4)
+        # save data
+        np.savez_compressed(data_save_path, x=x_sampled, y=y_sampled)
+
+        with open(metadata_save_path, 'w') as f:
+            json.dump(meta,f, indent=4)
 
 
 if __name__ == '__main__':
 
+    # configs
+    config = Config()
+
     # prepare all data splits
-    splits_to_prepare = ['train', 'val', 'train_val', 'test']
-    splits_to_sample = ['train', 'train_val']
+    splits_to_prepare = ['train', 'val' ,'test']
+    splits_to_sample = ['train', 'val']
     for split in splits_to_prepare:
         prepare_data(load_path=config.DATASET['unprocessed'][split],
                      target_preparing_path=config.DATASET['prepared'][split]['data'],
                      preparing_meta_path=config.DATASET['prepared'][split]['metadata'])
 
-    # sample train data
+    # sample train and val data
     for split in splits_to_sample:
         sample_save_data(load_prepared_path=config.DATASET['prepared'][split]['data'],
-                         load_meta_path=config.DATASET['prepared'][split]['metadata'],
-                         split_name=split)
+                         meta_path=config.DATASET['prepared'][split]['metadata'], config=config)
