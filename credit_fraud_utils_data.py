2736ed4823425ccfbe4282ed4d02a54e6b108fd2
Abdallah <abdallah.ashraf854@gmail.com>
2026-04-16 03:57:06 +0200

    updates

--- credit_fraud_utils_data.py
+++ credit_fraud_utils_data.py
@@ -1,35 +1,24 @@
 import json
 import pandas as pd
 import numpy as np
-from credit_fraud_utils_helper import get_processed_data
 from load_configs import Config
+from credit_fraud_utils_helper import load_data
 from imblearn.over_sampling import SMOTE
 from imblearn.under_sampling import RandomUnderSampler, EditedNearestNeighbours
 from imblearn.combine import SMOTEENN, SMOTETomek
 
-def load_data(path: str):
-    '''
-    Load dataset & split to X, y
-    :param path: data path
-    :return X: input
-    :return y: ground truth
-    '''
-    df = pd.read_csv(path)
-    X = df.drop(config.FEATURES['target'], axis=1)
-    y = df[config.FEATURES['target']]
-    return X, y
 
 
 def feature_construction(df:pd.DataFrame):
     '''
     Extracting new features according what concluded from EDA
     :param df: dataset
-    :return: None
+    :return: df
     '''
-
-    df['hour'] = ((df['Time'] // 3600) % 24).astype(int)  # hours
-    df['minute'] = ((df['Time'] // 60) % (24 * 60)).astype(int)  # minutes
-    df['second'] = ((df['Time']) % (24 * 60 * 60)).astype(int)  # seconds
+    df = df.copy()
+    df['hour'] = ((df['Time'] // 3600) % 48).astype(int)  # hours
+    df['minute'] = ((df['Time'] // 60) % 60).astype(int)  # minutes
+    df['second'] = ((df['Time']) % 60).astype(int)  # seconds
 
     # Day time periods
     df['morning'] = df['hour'].between(6, 11).astype(float)
@@ -40,62 +29,32 @@ def feature_construction(df:pd.DataFrame):
     # High amount feature
     df['high_amount'] = (df['Amount'] > 2000).astype(float)
 
+    return df
+
 
 def feature_transformation(df:pd.DataFrame):
     '''
     Apply feature transformation methods for specific features:
         1- log transformation for skewed data ('Amount')
     :param df: data frame
+    :return: df
     '''
 
-    for col in config.FEATURES['log_trans']:
-        df[col] = np.log1p(df[col])
+    # Log transformation for Amount feature
+    df = df.copy()
+    df['Amount'] = np.log1p(df['Amount'])
+    return df
 
 
-def prepare_data(load_path:str='', target_preparing_path:str='', preparing_meta_path:str=''):
-    '''
-    preparing methods (feature extraction - feature transformation)
-    :param load_path: data path
-    :param target_preparing_path: where prepared data will be saved
-    :param preparing_meta_path: where metadata of prepared data will be saved
-    :return: None
-    '''
-
-    # load data
-    X, y =  load_data(load_path)
-
-    # apply feature engineering
-    feature_construction(df=X)
-    feature_transformation(df=X)
-
-    # convert x & y to numpy array
-    x = X.to_numpy()
-    y = y.to_numpy()
-
-    # save data in .npz file
-    np.savez_compressed(target_preparing_path, x=x, y=y)
-
-    # metadata of prepared data
-    metadata= {
-        'cols_names' : X.columns.tolist(),
-        'n_samples': X.shape[0],
-        'n_features': X.shape[1],
-    }
-    # save in json file
-    with open(preparing_meta_path, 'w') as f:
-        json.dump(metadata, f, indent=4)
-
-
-def sample_save_data(load_prepared_path:str='', meta_path:str='',config=Config()):
+def sample_save_data(data_path:str='', config=Config()):
     '''
     apply all sample techniques for train split data and save for modeling fits
-    :param load_prepared_path: where prepared data saved
-    :param meta_path: path of metadata that contain cols names
+    :param data_path: where data is saved
     :param config: set configurations
     :return: None
     '''
 
-    X, t = get_processed_data(data_path=load_prepared_path, dtype='df', meta_path=meta_path)
+    X, t = load_data(data_path, target_col_name=config.FEATURES['target'])
 
     # get our configs
     kn = config.SAMPLING['params']['smote_kn']
@@ -132,14 +91,8 @@ def sample_save_data(load_prepared_path:str='', meta_path:str='',config=Config()
         }
 
         # save sampled data in .npz file
-        data_save_path = None
-        metadata_save_path = None
-        if 'train'  in load_prepared_path:
-            data_save_path = config.DATASET['sampled'][name]['train']['data']
-            metadata_save_path = config.DATASET['sampled'][name]['train']['metadata']
-        elif 'val' in load_prepared_path:
-            data_save_path = config.DATASET['sampled'][name]['val']['data']
-            metadata_save_path = config.DATASET['sampled'][name]['val']['metadata']
+        data_save_path = config.DATASET['sampled'][name]['train']['data']
+        metadata_save_path = config.DATASET['sampled'][name]['train']['metadata']
 
         # save data
         np.savez_compressed(data_save_path, x=x_sampled, y=y_sampled)
@@ -152,16 +105,9 @@ if __name__ == '__main__':
 
     # configs
     config = Config()
-
-    # prepare all data splits
-    splits_to_prepare = ['train', 'val' ,'test']
-    splits_to_sample = ['train', 'val']
-    for split in splits_to_prepare:
-        prepare_data(load_path=config.DATASET['unprocessed'][split],
-                     target_preparing_path=config.DATASET['prepared'][split]['data'],
-                     preparing_meta_path=config.DATASET['prepared'][split]['metadata'])
+    splits_to_sample = ['train']
 
     # sample train and val data
     for split in splits_to_sample:
-        sample_save_data(load_prepared_path=config.DATASET['prepared'][split]['data'],
-                         meta_path=config.DATASET['prepared'][split]['metadata'], config=config)
+        sample_save_data(data_path=config.DATASET['unprocessed'][split],
+                         config=config)
