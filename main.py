370c7397b207635f2e47d729a27f8e014926e9bc
Abdallah <abdallah.ashraf854@gmail.com>
2026-04-24 16:28:45 +0300

    update

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from credit_fraud_utils_helper import parse_arg, model_eval
 from credit_fraud_train import logistic_regression_model, random_forest_model, neural_network_classifier, \
-    neural_network_fl, voting_classifier, knn_classifier
-from credit_fraud_utils_helper import load_data
+    neural_network_fl, voting_classifier_1,voting_classifier_2,voting_classifier_3, knn_classifier
+from credit_fraud_utils_helper import load_data, compare_evals_retrain_best_model
 import torch
 import numpy as np
 from load_configs import Config
@@ -10,21 +10,26 @@ from load_configs import Config
 def main():
     # train dict models
     train = {
-        'lr' : logistic_regression_model,
-        'rf' : random_forest_model,
-        'nn' : neural_network_classifier,
+        'lr'    : logistic_regression_model,
+        'rf'    : random_forest_model,
+        'nn'    : neural_network_classifier,
         'nn_fl' : neural_network_fl,
-        'knn': knn_classifier,
-        'vc' : voting_classifier
+        'knn'   : knn_classifier,
+        'vc1'   : voting_classifier_1,
+        'vc2'   : voting_classifier_2,
+        'vc3'   : voting_classifier_3,
     }
     # evaluation dict models
     eval = {
-        'lr' : 'logistic_regression',
-        'rf' : 'random_forest',
-        'nn' : 'neural_network',
+        'lr'    : 'logistic_regression',
+        'rf'    : 'random_forest',
+        'nn'    : 'neural_network',
         'nn_fl' : 'neural_network_fl',
-        'knn': 'knn_classifier',
-        'vc' : 'voting_classifier'
+        'knn'   : 'knn_classifier',
+        'vc1'   : 'voting_classifier_1',
+        'vc2'   : 'voting_classifier_2',
+        'vc3'   : 'voting_classifier_3'
+
     }
 
     # argument parser
@@ -57,6 +62,9 @@ def main():
                    show_plot=show_plot, beta=config.EVALUATION['beta'],
                    plot_path=f'{config.EVALUATION['dir']}/pr-curve_{eval[arg.algorithm]}_{arg.sampling}.png')
 
+        compare_evals_retrain_best_model(config=config)
+
+
     elif arg.mode == 'full':
         train[arg.algorithm](sample_technique=arg.sampling, config=config)
 
@@ -67,6 +75,7 @@ def main():
                    show_plot=show_plot, beta=config.EVALUATION['beta'],
                    plot_path=f'{config.EVALUATION['dir']}/pr-curve_{eval[arg.algorithm]}_{arg.sampling}.png')
 
+        compare_evals_retrain_best_model(config=config)
 
 if __name__ == '__main__':
     main()
